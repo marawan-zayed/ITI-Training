7a5137fdd370ba00f4d7221a64563d398db19d95
marawan-zayed <marawan0111057772@gmail.com>
2026-08-23 20:58:19 +0300

    Day11 Uplouded

---
+++ Day 11/Task1/js/script.js
@@ -0,0 +1,18 @@
+// var firstName = "Marwan";
+// var age = 20;
+// var isStudent = true;
+// var x = undefined;
+// var y = null;
+
+var grade = window.prompt();
+if (grade > 90) {
+  console.log(`Excellent`);
+} else if (grade >= 80 && grade <= 89) {
+  console.log(`Good`);
+} else if (grade >= 70 && grade <= 79) {
+  console.log(`Averege`);
+} else if (grade >= 60 && grade <= 69) {
+  console.log(`Pass`);
+} else {
+  console.log(`Fail`);
+}
---
+++ Day 11/Task2/js/script.js
@@ -0,0 +1,10 @@
+var number = window.prompt();
+if (number > 0 && number % 2 == 0) {
+  console.log(`Number is positive and even`);
+} else if (number > 0 && number % 2 == 1) {
+  console.log(`Number is positive and odd`);
+} else if (number < 0) {
+  console.log(`Number is negative`);
+} else {
+  console.log(`number is zero`);
+}
